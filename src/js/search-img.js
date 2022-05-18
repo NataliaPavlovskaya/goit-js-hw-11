781401270a6372ea2237a68290361b7453d419af
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-05-18 20:19:35 +0300

    hw 11

---
+++ src/js/search-img.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default async function fetchImages(value, page) {
+  const url = 'https://pixabay.com/api/';
+  const key = '27468413-3d55e0b6bb58ee7723acedfd1';
+  const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+
+  return await axios.get(`${url}${filter}`)
+  .then(response => response.data);
+};
